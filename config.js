39cd808eba6086fd3c25b35d3a49fce8db31df9a
SlasherHere <145149276+SlasherHere@users.noreply.github.com>
2023-10-07 12:38:03 +0600

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.statusseen = true;                 //This is the new variable for control
 //
 global.Owner = ["8801633388307","8801975492880"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
 global.OwnerNumber = ["880633388307","8801975492880"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801633388307"];
+global.ownertag = ["8801633388307","8801975492880"];
 global.OwnerName = "Slasher";
 global.BotName = "XLICON-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
