4c39ff3cf79459d08a0565fe2fe1c22e99aa1b50
SlasherHere <145149276+SlasherHere@users.noreply.github.com>
2023-10-07 12:25:56 +0600

    Update config.js

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801975492880","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801975492880","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801975492880"];
+global.Owner = ["8801633388307","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["880633388307","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["8801633388307"];
 global.OwnerName = "Slasher";
 global.BotName = "XLICON-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
@@ -33,7 +33,7 @@ global.sessionName = "session";                          //Do not change.
 
 
 //
-global.prefa = ["."];                                    //Default prefix here.
+global.prefa = ["-"];                                    //Default prefix here.
 global.openAiAPI = "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";
 
 
